3fce6205fd1c1d923152c417fcce85503beb00de
HittmanA <hittmana@comcast.net>
2017-01-17 17:02:42 -0600

    Chunk generation update

--- server.js
+++ server.js
@@ -47,19 +47,20 @@ function genLoginWorld (chunkX, chunkZ) {
     var x, y, z;
     for (x = 0; x < 16; x++) {
         for (z = 0; z < 16; z++) {
-            //Bedrock layer
-            chunk.setBlockType(new Vector3(x, 0, z), 3);
-            chunk.setSkyLight(new Vector3(x, 0, z), 15);
-            chunk.setBlockType(new Vector3(x, 1, z), 2);
-            chunk.setSkyLight(new Vector3(x, 1, z), 15);
-
-            //Air layer
-            continue;
-            for (y = 2; y < 256; y++) {
-                chunk.setSkyLight(new Vector3(x, y, z), 15);
-                //chunk.setBlockLight(new Vector3(x, y, z), 15);
-                chunk.setBiomeColor(new Vector3(x, y, z), 141, 184, 113);
+          for (y = 0; y < 256; y++) {
+            if(y <= 2) {
+              //Bedrock layer
+              chunk.setBlockType(new Vector3(x, 0, z), 3);
+              chunk.setSkyLight(new Vector3(x, 0, z), 15);
+              chunk.setBlockType(new Vector3(x, 1, z), 2);
+              chunk.setSkyLight(new Vector3(x, 1, z), 15);
             }
+            if(y >= 3) {
+              chunk.setSkyLight(new Vector3(x, y, z), 15);
+              //chunk.setBlockLight(new Vector3(x, y, z), 15);
+              chunk.setBiomeColor(new Vector3(x, y, z), 141, 184, 113);
+            }
+          }
         }
     }
 
