e6510ead1f8600497453f610636560b2b7395f5c
Ron Friedman <ron852719@icloud.com>
2017-01-22 15:52:50 -0800

    Spacing changes

--- nmps/Console/Console.js
+++ nmps/Console/Console.js
@@ -2,38 +2,37 @@ var colors = require("../Chat/Colors.js");
 colors = new colors();
 var log = console.log;
 console.log = function(message, logtrue) {
-  if(logtrue === false || logtrue == undefined) {
-    log(message);
-  }else{
-    var timestamp = require("../Time/Time.js");
-    log("[" + timestamp.getTimestamp() + "][INFO] " + message);
-  }
+    if (logtrue === false || logtrue == undefined) {
+        log(message);
+    } else {
+        var timestamp = require("../Time/Time.js");
+        log("[" + timestamp.getTimestamp() + "][INFO] " + message);
+    }
 }
 
 module.exports = {
-  info: function(message) {
-    var timestamp = require("../Time/Time.js");
-    console.log("[" + timestamp.getTimestamp() + "][INFO] " + message);
-  },
+        info: function(message) {
+            var timestamp = require("../Time/Time.js");
+            console.log("[" + timestamp.getTimestamp() + "][INFO] " + message);
+        },
 
-  warn: function(message) {
-    var timestamp = require("../Time/Time.js");
-    console.log(colors.terminal.yellow.bold("[" + timestamp.getTimestamp() + "][WARNING] " + message));
-  },
+        warn: function(message) {
+            var timestamp = require("../Time/Time.js");
+            console.log(colors.terminal.yellow.bold("[" + timestamp.getTimestamp() + "][WARNING] " + message));
+        },
 
-  critical: function(message) {
-    var timestamp = require("../Time/Time.js");
-    console.log(colors.terminal.bgRed.bold("[" + timestamp.getTimestamp() + "][CRITICAL] " + message));
-  },
+        critical: function(message) {
+            var timestamp = require("../Time/Time.js");
+            console.log(colors.terminal.bgRed.bold("[" + timestamp.getTimestamp() + "][CRITICAL] " + message));
+        },
 
-  error: function(error, info) {
-    var timestamp = require("../Time/Time.js");
-    var errorMessage = require("../Error/ErrorManager");
-    console.log(colors.terminal.red.bold("[" + timestamp.getTimestamp() + "][ERROR] " + errorMessage.getMessageFromError(error, info)));
-  },
-  
-  setup: function(message) {
-    var timestamp = require("../Time/Time.js");
-    console.log("[" + timestamp.getTimestamp() + "][SETUP] " + message);
-  }
-}
+        error: function(error, info) {
+            var timestamp = require("../Time/Time.js");
+            var errorMessage = require("../Error/ErrorManager");
+            console.log(colors.terminal.red.bold("[" + timestamp.getTimestamp() + "][ERROR] " + errorMessage.getMessageFromError(error, info)));
+        },
+
+        setup: function(message) {
+            var timestamp = require("../Time/Time.js");
+            console.log("[" + timestamp.getTimestamp() + "][SETUP] " + message);
+        }
