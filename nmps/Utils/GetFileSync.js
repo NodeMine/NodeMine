8dd6860c30e8e07417ee0417d11f911dd85c91c7
Hampton <herohamp@users.noreply.github.com>
2017-01-22 18:16:09 -0500

    Update GetFileSync.js

--- nmps/Utils/GetFileSync.js
+++ nmps/Utils/GetFileSync.js
@@ -1,10 +1,10 @@
 module.exports = {
-  getFileSync: function(file,options) {
-    var fs = require("fs");
-    if(options == undefined || options == null) {
-      return fs.readFileSync(file).toString();
-    }else{
-      return fs.readFileSync(file, options).toString();
-    }
-  }
+	getFileSync: function(file, options) {
+		var fs = require("fs");
+		if (options == undefined || options == null) {
+			return fs.readFileSync(file).toString();
+		} else {
+			return fs.readFileSync(file, options).toString();
+		}
+	}
 }
