a41f4f8e1cbea8557b6c2a7188159dbfd669a767
Ron Friedman <ron852719@icloud.com>
2017-01-22 15:23:04 -0800

    Clean up spacing

--- nmps/Command/CommandParser.js
+++ nmps/Command/CommandParser.js
@@ -1,13 +1,13 @@
 module.exports = function() {
-  var events = require("../Events/EventEmitter.js");
-  events.on("CONSOLE_INPUT", function(data){
-    if(data[0] == "/") {
-      data = data.replace("/","").trim();
-      data = data.split(" ");
-      command = data.shift();
-      events.emit("COMMAND", command, data);
-    }else{
-      //Send to chat
-    }
-  });
+    var events = require("../Events/EventEmitter.js");
+    events.on("CONSOLE_INPUT", function(data) {
+        if (data[0] == "/") {
+            data = data.replace("/", "").trim();
+            data = data.split(" ");
+            command = data.shift();
+            events.emit("COMMAND", command, data);
+        } else {
+            //Send to chat
+        }
+    });
 }
