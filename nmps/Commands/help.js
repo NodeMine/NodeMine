fbaac88cfb5685f010faffb360070b3e5d746d56
Ron Friedman <ron852719@icloud.com>
2017-01-22 15:27:27 -0800

    Clean up spacing

--- nmps/Commands/help.js
+++ nmps/Commands/help.js
@@ -2,10 +2,10 @@ var commands = require("../Data/commands.json");
 var logger = require("../Console/Console.js");
 var events = require("../Events/EventEmitter.js");
 
-events.on("COMMAND", function(command, args){
-  if(command == "help") {
-    for(i in commands) {
-      logger.info("/" + i + ":\n\t\t  " + commands[i]["help"] + "\n\t\t  " + commands[i]["role"]);
+events.on("COMMAND", function(command, args) {
+    if (command == "help") {
+        for (i in commands) {
+            logger.info("/" + i + ":\n\t\t  " + commands[i]["help"] + "\n\t\t  " + commands[i]["role"]);
+        }
     }
-  }
 });
