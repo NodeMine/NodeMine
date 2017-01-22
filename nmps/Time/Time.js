b3595b7ad4a462dfc91cab16bbe8b62e06aac5a5
Hampton <herohamp@users.noreply.github.com>
2017-01-22 18:16:31 -0500

    Update Time.js

--- nmps/Time/Time.js
+++ nmps/Time/Time.js
@@ -1,10 +1,10 @@
 module.exports = {
-  getTimestamp: function() {
-    var d = new Date();
-    var seconds = d.getSeconds();
-    var minutes = d.getMinutes();
-    var hours = d.getHours();
-    var timestamp = hours + ":" + minutes + ":" + seconds;
-    return timestamp;
-  }
+	getTimestamp: function() {
+		var d = new Date();
+		var seconds = d.getSeconds();
+		var minutes = d.getMinutes();
+		var hours = d.getHours();
+		var timestamp = hours + ":" + minutes + ":" + seconds;
+		return timestamp;
+	}
 }
